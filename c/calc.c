418c9cedc3b3dc5fa94b898cec32fb9bd5123bac
hxvrmxnv <leonardigit@gmail.com>
2022-09-23 17:11:30 -0300

    add calc.c

---
+++ c/calc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int num, _num, __num, result;
+
+  printf("Digite os valores: ");
+  scanf("%d%d%d", &num, &_num, &__num);
+
+
+  printf("Sum: %d\n",result = (num + _num + __num));
+
+  return 0;
+}
