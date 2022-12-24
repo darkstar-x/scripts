bca018e0b0986b30cc8633c3a825c378ac625d59
s4ymon <leonardigit@gmail.com>
2022-12-24 15:00:54 -0300

    remove other scripts

--- sh/frost-xbps.sh
+++ sh/frost-xbps.sh
@@ -1,46 +1,52 @@
 #!/usr/bin/env bash
 
-##########################################
-# Author: L3ON4RDI                       #
-#  Description: This program is a menu   #
-#  for package manger XBPS on Void Linux #
-##########################################
-#
+#########################################
+# Author: Leonardi Melo                 #
+# Github: darkstar-x                    #
+# Youtube: Darkstar                     #
+# Description: This program is a menu   #
+# for package manger XBPS on Void Linux #
+#########################################
 
-source colors
-clear
+# COLORS
+RED=$'\e[0;31m'
+GRN=$'\e[0;32m'
+YLW=$'\e[0;33m'
+BLU=$'\e[0;34m'
+MAG=$'\e[0;35m'
+CYN=$'\e[0;46m'
+RST=$'\e[0m'
 
-echo -e "${Green} FXBPS${Off} Menu\n"
+# Performance for UTF-8
+export LC_ALL=C
+export LANG=C
 
-echo -e " 1 - Install packge\n 2 - Remove packes\n 3 - Search Package\n 4 - Update System\n 5 - Quit"
+MAIN_SCREEN() {
+  clear
+  cat << EOF
+  ${GRN}FXBPS Cli-menu${RST}
+    0 - Install packges
+    1 - Remove packges
+    2 - Update packges
+    3 - Remove orphans packges
+    4 - Update system
+    5 - Quit
+EOF
 
-read -r -p "-> " user_options
+  CHECK_OPTIONS
+}
 
-### Conditions
-case "$user_options" in
-  1)
-    read -r -p "Verbose mode (y)es or (n)o ? " verbose_mode
-    read -r -p "Please your package name: " package_name
-    if [[ ${verbose_mode} = "y" || ${verbose_mode} = "Y" ]]; then
-      echo -e "$Green Installing ${package_name} $Off "
-      echo "====> VB on"
-      #sudo xbps-install -Suvy ${package_name}
-    else
-      echo -e "$Green Installing ${package_name} $Off "
-      #sudo xbps-install --Suy ${package_name}
-    fi
-  ;;
-  2)
-    echo "Two option"
-  ;;
-  3)
-    echo "three option"
-  ;;
-  4)
-    echo "Fuour option"
-  ;;
-  5)
-    echo "Exiting ..."
-    exit 0
-  ;;
-esac
+CHECK_OPTIONS() {
+  read -r -p "Chose your option: " chose_user
+  read -r -p "Verbose (Y)es or (N)o? " verbose_mode 
+
+  if [ $verbose_mode == "Y" ]; do
+    verbose="true"
+  fi
+
+  case $chose_user in
+    1)
+      echo -e "install-info"
+}
+
+MAIN_SCREEN
